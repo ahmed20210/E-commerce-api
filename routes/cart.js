ee89f4dc72bee236c506b8a639b47b016236fb1c
ahmed <ahmed@debian.ahmed>
2022-08-07 14:58:59 +0200

    ain

--- routes/cart.js
+++ routes/cart.js
@@ -160,6 +160,7 @@ router.post("/change", async (req, res) => {
   if (validate.error) {
     res.sendStatus(400);
   } else {
+    console.log("first")
     const result = await changeQuantity(userId, productId, quantity);
     checkDone(res, result);
   }
